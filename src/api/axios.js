902923800debe66e911f58214363524b959b1d52
Harsh Anand <harsh25061999@gmail.com>
2026-08-07 20:53:00 +0530

    Add VITE_API_URL base URL to axios instance

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+
 const api = axios.create({
+  baseURL: API_BASE,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
